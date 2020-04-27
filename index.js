09acbb8e3bb9f509130a3377150fdebc93d0fa7c
lemonWM <arturchochla@gmail.com>
2020-04-27 14:44:30 +0200

    make it better

--- index.js
+++ index.js
@@ -1,46 +1,44 @@
 const express = require('express')
 const app = express();
 const path = require('path')
+const mongo = require('mongodb')
+const MongoClient = mongo.MongoClient
+const ObjectId = require('mongodb').ObjectID
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const PORT = process.env.PORT || 5000
+
+dbUrl = 'mongodb://mo1030_appTravel:Lemon2@mongo26.mydevil.net:27017/mo1030_appTravel'
+urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 app.use( cors() );
 app.use( bodyParser.json() );
 app.use( express.static("public") );
 
-app.get("/questions", function(req, res) {
 
-    const questions = require("./data/questions.json")
-        .map(q => ({
-            id: q.id,
-            question: q.question,
-            answers: q.answers
-        }));
+app.get('/places', function(req, res){
+    
+    MongoClient.connect(dbUrl, function(err, db) { 
 
-    res.json(questions);
+        if(err) {
+            res.status(500);
+            res.send({error: true});
 
-});
+            return;
+        }
+    
+        db.collection("places").find({}).toArray(function(err, docs) {
+              
+            if(err) {     
+                res.status(500);
+                res.json({error: true});
 
-app.post("/check", function(req, res) {
+                return;
+            }
+            res.json(docs)
+            db.close()
+        })
+    })
+})
 
-    const questions = require("./data/questions.json");
-    const answers = req.body.answers;
-
-    let result = questions.map(question => {
-        const answer = answers[question.id];
-
-        return {
-            id: question.id,
-            correct: answer === question.correct,
-            question: question.question,
-            yourAnswer: question.answers[answer],
-            correctAnswer: question.answers[question.correct]
-        };
-    });
-
-    res.json(result);
-
-});
-
-  app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
