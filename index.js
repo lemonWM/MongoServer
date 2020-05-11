599a09c98616e9183f0840cf9ce0a79f6a09ea88
lemonWM <arturchochla@gmail.com>
2020-05-11 19:52:52 +0200

    make it better

--- index.js
+++ index.js
@@ -6,9 +6,19 @@ const MongoClient = mongo.MongoClient
 const ObjectId = require('mongodb').ObjectID
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
+const fs = require("fs");
+const low = require("lowdb");
+const FileSync = require("lowdb/adapters/FileSync");
+const shortid = require("shortid");
+const slugify = require("slugify");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const jwtVerify = require("express-jwt");
+const SECRET_KEY = fs.readFileSync("private.key");
 
-dbUrl = 'mongodb://mo1030_appTravel:Lemon2@mongo26.mydevil.net:27017/mo1030_appTravel'
+
+dbUrl = 'mongodb://mo1030_traveo:Lemon2@mongo26.mydevil.net:27017/mo1030_traveo'
 urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 app.use( cors() );
@@ -16,6 +26,7 @@ app.use( bodyParser.json() );
 app.use( express.static("public") );
 
 
+/* Routes */
 app.get('/places', function(req, res){
     
     MongoClient.connect(dbUrl, function(err, db) { 
@@ -40,5 +51,34 @@ app.get('/places', function(req, res){
         })
     })
 })
+
+
+app.get('/articles', function(req, res){
+    
+    MongoClient.connect(dbUrl, function(err, db) { 
+
+        if(err) {
+            res.status(500);
+            res.send({error: true});
+
+            return;
+        }
+    
+        db.collection("articles").find({}).toArray(function(err, docs) {
+              
+            if(err) {     
+                res.status(500);
+                res.json({error: true});
+
+                return;
+            }
+            res.json(docs)
+            db.close()
+        })
+    })
+})
+
+
+
 
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
